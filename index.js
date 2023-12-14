ed366030793f3bcdea0bbbbcd28735cfd1869079
Patricia Frogoso <pfrogoso@gmail.com>
2023-12-14 13:27:04 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+let num3 = 10
+let num4 = 6
+let mod = num3 % num4;
+const max = Math.max(1,20);
 
-// Write your code here
